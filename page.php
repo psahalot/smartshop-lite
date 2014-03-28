fe1085c63a550887c73132c9b40afcbb0153a770
Puneet Sahalot <puneetsahalot@gmail.com>
2014-03-29 04:25:33 +0530

    added missing comment form function

--- page.php
+++ page.php
@@ -1,32 +1,37 @@
 <?php get_header(); ?>
 
 <div id="page-header-container" class="container">
-     <div class="headsection row">
+    <div class="headsection row">
         <h2 class="title"><?php the_title(); ?></h2>
     </div>
 </div>
 <div id="main-content-container" class="container">    
     <div id="main-content" class="row">	
-    <div class="col grid_8_of_12">	
-        <div class="content clearfix">
+        <div class="col grid_8_of_12">	
+            <div class="content clearfix">
 
-            <?php while (have_posts()) : the_post(); ?>
+                <?php while (have_posts()) : the_post(); ?>
 
-                <div <?php post_class(); ?>"  id="post-<?php the_ID(); ?>">
+                    <div <?php post_class(); ?>"  id="post-<?php the_ID(); ?>">
 
-                    <?php the_content(); ?>
+                        <?php the_content(); ?>
 
-                <?php endwhile; ?>
+                    <?php endwhile; ?>
 
-            </div><!--end .entry-->
+                </div><!--end .entry-->
 
-            <div class="page-pagination">
-                <?php wp_link_pages('before=<p>&after=</p>&next_or_number=number&pagelink=page %'); ?>
-            </div><!-- end .pagination-->
-        </div><!--end .content-->
-        
-        <div class="comments">
-            <?php comments_template(); ?>
+                <div class="page-pagination">
+                    <?php wp_link_pages('before=<p>&after=</p>&next_or_number=number&pagelink=page %'); ?>
+                </div><!-- end .pagination-->
+            </div><!--end .content-->
+
+            <div class="comments">
+                <?php
+                // If comments are open or we have at least one comment, load up the comment template
+                if (comments_open() || '0' != get_comments_number()) {
+                    comments_template('', true);
+                }
+                ?>
         </div>
         
     </div><!--end .col grid_8_of_12-->
--- single.php
+++ single.php
@@ -7,54 +7,59 @@
 </div>
 <div id="main-content-container" class="container">     
     <div id="main-content" class="row">	
-    
-    <div class="col grid_8_of_12">		
-        <div class="content">
-            <?php if (have_posts()) : ?>
 
-                <?php while (have_posts()) : the_post(); ?>
+        <div class="col grid_8_of_12">		
+            <div class="content">
+                <?php if (have_posts()) : ?>
 
-                    <div <?php post_class(); ?>"  id="post-<?php the_ID(); ?>">
+                    <?php while (have_posts()) : the_post(); ?>
 
-                        <?php
-                        if ('' != get_the_post_thumbnail()) {
-                            the_post_thumbnail();
-                        }
-                        ?> 
-                        <?php the_content('Read the rest of this entry &raquo;'); ?>
+                        <div <?php post_class(); ?>"  id="post-<?php the_ID(); ?>">
 
-                        <p class="postinfo">
-                            <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-                        </p>
-                        
-                    </div>
+                            <?php
+                            if ('' != get_the_post_thumbnail()) {
+                                the_post_thumbnail();
+                            }
+                            ?> 
+                            <?php the_content('Read the rest of this entry &raquo;'); ?>
 
+                            <p class="postinfo">
+                                <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+                            </p>
 
-                <?php endwhile; ?>
-
-            <?php else : ?>
-
-                <div class="entry">
-                    <h2 class="title">Not Found</h2>
-                    <p>Sorry, but you are looking for something that isn't here.</p>
-                    <?php get_search_form(); ?>
-                </div><!--end .entry-->
-
-            <?php endif; ?>
-
-        </div><!--end .content-->
+                        </div>
 
 
-        <div class="comments">
-            <?php comments_template(); ?>
-        </div>
-    </div><!--end .col grid_8_of_12-->
+                    <?php endwhile; ?>
 
-    <div class="col grid_4_of_12 last right-sidebar">
-        <?php get_sidebar(); ?>
-    </div><!--end .col grid_4_of_12-->		
+                <?php else : ?>
 
-</div><!--end .row#main-content-->
+                    <div class="entry">
+                        <h2 class="title">Not Found</h2>
+                        <p>Sorry, but you are looking for something that isn't here.</p>
+                        <?php get_search_form(); ?>
+                    </div><!--end .entry-->
+
+                <?php endif; ?>
+
+            </div><!--end .content-->
+
+
+            <div class="comments">
+                <?php
+                // If comments are open or we have at least one comment, load up the comment template
+                if (comments_open() || '0' != get_comments_number()) {
+                    comments_template('', true);
+                }
+                ?>
+            </div>
+        </div><!--end .col grid_8_of_12-->
+
+        <div class="col grid_4_of_12 last right-sidebar">
+            <?php get_sidebar(); ?>
+        </div><!--end .col grid_4_of_12-->		
+
+    </div><!--end .row#main-content-->
 </div><!-- end #main-content-container -->
 
 <?php get_footer(); ?>
