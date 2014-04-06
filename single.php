2ff4b3b2627c6ede2ca101937ecf2c6c75a9413a
Puneet Sahalot <puneetsahalot@gmail.com>
2014-04-07 01:37:10 +0530

    fixed theme submission issues

--- single.php
+++ single.php
@@ -21,10 +21,10 @@
                                 the_post_thumbnail();
                             }
                             ?> 
-                            <?php the_content('Read the rest of this entry &raquo;'); ?>
+                            <?php the_content('Read the rest of this entry &raquo;','smartshop'); ?>
 
                             <p class="postinfo">
-                                <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+                                <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;','smartshop'); ?>
                             </p>
 
                         </div>
