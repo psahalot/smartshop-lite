978abf184c387c58364128f23964a899bb810627
Puneet Sahalot <puneetsahalot@gmail.com>
2014-04-10 02:46:12 +0530

    added localization strings

--- includes/plugin-activation/plugin-details.php
+++ includes/plugin-activation/plugin-details.php
@@ -7,7 +7,7 @@
  * functions.php file, and amend to suit.
  *
  * @package    TGM-Plugin-Activation
- * @subpackage Example
+ * @subpackage SmartShop
  * @version    2.4.0
  * @author     Thomas Griffin <thomasgriffinmedia.com>
  * @author     Gary Jones <gamajo.com>
@@ -21,7 +21,7 @@
  */
 require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
-add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+add_action( 'tgmpa_register', 'smartshop_register_required_plugins' );
 /**
  * Register the required plugins for this theme.
  *
@@ -34,7 +34,7 @@ add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
  * This function is hooked into tgmpa_init, which is fired within the
  * TGM_Plugin_Activation class constructor.
  */
-function my_theme_register_required_plugins() {
+function smartshop_register_required_plugins() {
 
     /**
      * Array of plugin arrays. Required keys are name and slug.
@@ -44,21 +44,21 @@ function my_theme_register_required_plugins() {
 
         // This is used to install Soliloquy Slider plugin from WordPress Plugin Repository.
         array(
-            'name'               => 'Soliloquy Lite', // The plugin name.
+            'name'               => __('Soliloquy Lite','smartshop'), // The plugin name.
             'slug'               => 'soliloquy-lite', // The plugin slug (typically the folder name).
             'required'           => false, // If false, the plugin is only 'recommended' instead of required.
         ),
 
         // This is to install Contact Form 7 from WordPress Plugin Repository.
         array(
-            'name'      => 'Contact Forms 7',
+            'name'      => __('Contact Form 7','smartshop'),
             'slug'      => 'contact-form-7',
             'required'  => false,
         ),
 
          // This is to install Envira Lite from WordPress Plugin Repository.
         array(
-            'name'      => 'Envira Gallery Lite',
+            'name'      => __('Envira Gallery','smartshop'),
             'slug'      => 'envira-gallery-lite',
             'required'  => false,
         ),
@@ -80,23 +80,23 @@ function my_theme_register_required_plugins() {
         'is_automatic' => false,                   // Automatically activate plugins after installation or not.
         'message'      => '',                      // Message to output right before the plugins table.
         'strings'      => array(
-            'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
-            'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
-            'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
-            'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
-            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
-            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
-            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
-            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
-            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
-            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
-            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
-            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
-            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
-            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
-            'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
-            'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
-            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+            'page_title'                      => __( 'Install Required Plugins', 'smartshop' ),
+            'menu_title'                      => __( 'Install Plugins', 'smartshop' ),
+            'installing'                      => __( 'Installing Plugin: %s', 'smartshop' ), // %s = plugin name.
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'smartshop' ),
+            'notice_can_install_required'     => _n_noop( __('This theme requires the following plugin: %1$s.','smartshop'), __('This theme requires the following plugins: %1$s.','smartshop')), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( __('This theme recommends the following plugin: %1$s.','smartshop'), __('This theme recommends the following plugins: %1$s.','smartshop')), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( __('Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.','smartshop'), __('Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.','smartshop')), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( __('The following required plugin is currently inactive: %1$s.','smartshop'), __('The following required plugins are currently inactive: %1$s.' ,'smartshop')), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( __('The following recommended plugin is currently inactive: %1$s.','smartshop'), __('The following recommended plugins are currently inactive: %1$s.' ,'smartshop')), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( __('Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.','smartshop'), __('Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.','smartshop')), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( __('The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.','smartshop'), __('The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ,'smartshop')), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( __('Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.','smartshop'), __('Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.','smartshop')), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( __('Begin installing plugin','smartshop'), __('Begin installing plugins','smartshop')),
+            'activate_link'                   => _n_noop( __('Begin activating plugin','smartshop'), __('Begin activating plugins' ,'smartshop')),
+            'return'                          => __( 'Return to Required Plugins Installer', 'smartshop' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'smartshop' ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'smartshop' ), // %s = dashboard link.
             'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
         )
     );
