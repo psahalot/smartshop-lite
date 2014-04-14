b8ea2f45343ed3c993f1dc0c2fe71c8e80cc7217
Puneet Sahalot <puneetsahalot@gmail.com>
2014-04-14 15:24:04 +0530

    updated front page template to front-page.php file

--- sidebar-front.php
+++ sidebar-front.php
@@ -8,8 +8,7 @@
  */
 // Check if home featured sidebar is active. Yes, then output the relevant HTML else exit. 
 
-if (is_active_sidebar('home-featured')) {
-    ?>
+?>
 
     <div id="home-featured-area" class="container" >
 
@@ -17,7 +16,17 @@ if (is_active_sidebar('home-featured')) {
 
             <div class="col grid_12_of_12 last">
 
-                <?php dynamic_sidebar('home-featured'); ?>
+                <?php
+                if (is_active_sidebar('home-featured')) {
+                    dynamic_sidebar('home-featured');
+                } 
+                else { ?>
+                    <div id="text-9" class="widget widget_text">			
+                        <div class="textwidget">
+                            <img src="http://localhost/smartshop/wp-content/uploads/2014/03/stairs-1140x450.jpg">
+                        </div>
+                    </div>
+                <?php } ?>
 
             </div>
 
@@ -25,56 +34,79 @@ if (is_active_sidebar('home-featured')) {
 
     </div> <!--end .container -->
 
-    <?php
-}
 
 
-// check if any of the three Home #1, #2 or #3 sidebars is active
-if (is_active_sidebar('home_one') || is_active_sidebar('home_two') || is_active_sidebar('home_three')) {
-    ?>
     <div id="home-widgets-area" class="container">
         <div class="row" id="home-widgets">
 
-            <?php if (is_active_sidebar('home_one')) { ?>
+            
             <div class="col grid_4_of_12 home-widget-one">
-                <?php dynamic_sidebar('home_one'); ?>
-
+                <?php if (is_active_sidebar('home_one')) { 
+                    dynamic_sidebar('home_one'); 
+                } 
+                else { ?>
+                <div class="home-widget widget_text">			
+                        <div class="textwidget">
+                            <p><i class="fa fa-gears"></i></p>
+                            <center>Home #1 Widget</center>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
-        <?php } ?>
-
-        <?php if (is_active_sidebar('home_two')) { ?>
-
+       
             <div class="col grid_4_of_12 home-widget-two">
-                <?php dynamic_sidebar('home_two'); ?>
+                <?php
+                if (is_active_sidebar('home_two')) {
+                    dynamic_sidebar('home_two');
+                } else { ?>
+                    <div class="home-widget widget_text">			
+                        <div class="textwidget">
+                            <p><i class="fa fa-comments"></i></p>
+                            <center>Home #2 Widget</center>
+                        </div>
+                    </div>
+            <?php } ?>
             </div>
-        <?php } ?>
-
-        <?php if (is_active_sidebar('home_three')) { ?>
-
-            <div class="col grid_4_of_12 home-widget-three last">
-                <?php dynamic_sidebar('home_three'); ?>
+            <div class="col grid_4_of_12 home-widget-three">
+                <?php
+                if (is_active_sidebar('home_three')) {
+                    dynamic_sidebar('home_three');
+                } else { ?>
+                    <div class="home-widget widget_text">			
+                        <div class="textwidget">
+                            <p><i class="fa fa-laptop"></i></p>
+                            <center>Home #3 Widget</center>
+                        </div>
+                    </div>
+            <?php } ?>
             </div>
-        <?php } ?>
 
     </div>
 </div>
-<?php } 
 
-    
-
-// Check if home CTA sidebar is active 
-if (is_active_sidebar('home_cta')) {
-    ?>
+ 
     <div id="home-cta-area" class="container" >
 
         <div class="row" id="home-cta">
 
             <div class="col grid_12_of_12">
-                <?php dynamic_sidebar('home_cta'); ?>
+                   
+            <?php 
+            // Check if home CTA sidebar is active 
+                if (is_active_sidebar('home_cta')) { 
+                    dynamic_sidebar('home_cta');
+                } else { ?>
+                    <div class="home-cta-widget widget_text">
+                        <h3 class="widget_title">Home CTA Widget</h3>			
+                        <div class="textwidget">
+                            <p>This is Home CTA widget area to add some Call to Action text and button</p>
+                            <p><a href="#" class="smartshop-cta">Let's get started</a></p>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
 
         </div> <!--end .row --> 
 
     </div> <!--end .container -->
-    <?php
-} ?>
+   
