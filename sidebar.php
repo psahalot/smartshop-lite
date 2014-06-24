f91a6893eb708e74f6df080797d99a09b5ad76fa
jainnidhi <jainnidhi_3@ymail.com>
2014-06-24 14:34:50 +0530

    updated files

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@
         if (class_exists('Easy_Digital_Downloads')) {
         if (!is_active_sidebar('sidebar_right') && !is_page('checkout')) {
             echo '<li class="widget edd-cart-widget">';
-            echo '<h3 class="widget_title">Shopping Cart</h3>';
+            echo '<h3 class="widget_title">'._e('Shopping Cart','smartshop').'</h3>';
             echo edd_shopping_cart();
             echo '</li>';
         }
