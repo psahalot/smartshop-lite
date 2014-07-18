1a6631a53d443d705b96b623f95a193bbf7cc642
Puneet Sahalot <puneetsahalot@gmail.com>
2014-07-18 14:18:13 +0530

    fixed invalide markup

--- sidebar.php
+++ sidebar.php
@@ -1,27 +1,27 @@
 <div class="sidebar">
-    <ul>
+    
         <?php
         if (class_exists('Easy_Digital_Downloads')) {
             if (is_page('checkout')) {
-            echo '<li class="widget edd-cart-widget">';
+            echo '<div class="widget edd-cart-widget">';
             echo '<h3 class="widget_title">'._e('Shopping Cart','smartshop').'</h3>';
             echo edd_shopping_cart();
-            echo '</li>';
+            echo '</div>';
         }
         }
 
-        if (is_active_sidebar('sidebar_right')) {
-            dynamic_sidebar('sidebar_right');
-        }
 
         if (class_exists('Easy_Digital_Downloads')) {
         if (!is_active_sidebar('sidebar_right') && !is_page('checkout')) {
-            echo '<li class="widget edd-cart-widget">';
+            echo '<div class="widget edd-cart-widget">';
             echo '<h3 class="widget_title">'._e('Shopping Cart','smartshop').'</h3>';
             echo edd_shopping_cart();
-            echo '</li>';
+            echo '</div>';
         }
         }
         ?>
-    </ul>
+    
+     <?php if (is_active_sidebar('sidebar_right')) {
+            dynamic_sidebar('sidebar_right');
+        } ?>
 </div>
